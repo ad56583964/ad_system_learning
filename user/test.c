fbf15843bb2f55205f8a6a5b4f1cbe0556ba3df7
ad56583964 <1176949486@qq.com>
2022-01-26 01:21:52 -0800

    test pipe close

--- user/test.c
+++ user/test.c
@@ -1,96 +1,58 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "user/pa_util.h"
-void concurrent_prime_recur(int pip_read_fd);
-void concurrent_prime(int num);
+
+enum{
+    PIPE_READ,
+    PIPE_WRITE
+};
+
+void readOut(int read_pipe)
+{
+    int read_buf;
+
+    int check_read = 1;
+    while(1)
+    {
+        if(check_read == 0)
+        {
+            printf("Finished\n");
+            break;
+        }
+        check_read = read(read_pipe,&read_buf,sizeof(int));
+        printf("Read: %d, Check: %d\n",read_buf,check_read);
+    }
+}
+
+void pushInNum(int write_pipe)
+{
+    for(int i = 0; i <= 5; i++)
+    {
+        write(write_pipe,&i,sizeof(int));
+    }
+    close(write_pipe);
+}
+
+
+void parent(int write_pipe)
+{
+    // for(int i = 0; i <= 5; i++)
+    // {
+    //     write(write_pipe,&i,sizeof(int));
+    // }
+}
 
 int
 main(int argc, char *argv[])
 {
-//     Use pipe to create a pipe.
-// Use fork to create a child.
-// Use read to read from the pipe, and write to write to the pipe.
-// Use getpid to find the process ID of the calling process.
-// Add the program to UPROGS in Makefile.
-// User programs on xv6 have a limited set of library functions available to them. You can see the list in user/user.h; the source (other than for system calls) is in user/ulib.c, user/printf.c, and user/umalloc.c.
-    
-    // Write a concurrent version of prime sieve using pipes.
-    //  This idea is due to Doug McIlroy, 
-    //  inventor of Unix pipes.
-    //   The picture halfway down this page and the
-    //    surrounding text explain how to do it. 
-    //    Your solution should be in the file user/primes.c.
-    if(argc>1){
-        int num=atoi(argv[1]);
-        printf("num is %d\n",num);
-        if(num!=-1){
-            //has num
-            concurrent_prime(num);
-        }
-    }
-    exit(0);
-}
-
-void concurrent_prime(int num){
     int p[2];
+
     pipe(p);
-
-    int a=fork();
     
-    if(a==0){
-        concurrent_prime_recur(p[Pipe_Read_Id]);
-    }else{
-        int elem=2;
-        printf("prime %d\n",2);
-        for(int i=3;i<=num;i++){
-            if(i%elem!=0){
-                printf("write %d\n",i);
-                write(p[Pipe_Write_Id],&i,sizeof(i));
-            }else{
-                // printf("no write %d\n",i);
-            }
-            sleep(1);
-        }
-    }
-}
+    pushInNum(p[PIPE_WRITE]);
 
-void concurrent_prime_recur(int pip_read_fd){
-    // printf("con\n");
-    int elem;
-    read(pip_read_fd,&elem,sizeof(int));
-    printf("prime %d\n",elem);
-    int elem2;
-    sleep(1);
-    read(pip_read_fd,&elem2,sizeof(int));
-    printf("read %d\n",elem2);
-//     if(elem2==-1){
-        
-//         //end
-//         return;
-//     }else{
-//         //continue split
-//         int p[2];
-//         pipe(p);
+    readOut(p[PIPE_READ]);
 
-//         int a=fork();
-        
-//         if(a==0){
-//             concurrent_prime_recur(p[Pipe_Read_Id]);
-//         }else{
-// __read:
-//             sleep(1);
-//             read(pip_read_fd,&elem2,sizeof(elem2));
-//             if(elem2==-1){
-//                 write(p[Pipe_Write_Id],&elem2,sizeof(elem2)); 
-//             }else{
-//                 if(elem2%elem!=0){
-//                     write(p[Pipe_Write_Id],&elem2,sizeof(elem2)); 
-//                 }
-//                 goto __read;
-//             }
-//         }
-//     }
-    
+    exit(0);
 }
 
