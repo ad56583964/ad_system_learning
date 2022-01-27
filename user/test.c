e070e7e2951eb5141e13704e658d3be59889cec9
ad56583964 <1176949486@qq.com>
2022-01-27 01:44:11 -0800

    tested console

--- user/test.c
+++ user/test.c
@@ -1,21 +1,17 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fcntl.h"
 
 int
 main(int argc, char *argv[])
 {
-//   int i;
-
-
-    printf("argc: %d",argc);  
-//   for(i = 1; i < argc; i++){
-//     write(1, argv[i], strlen(argv[i]));
-//     if(i + 1 < argc){
-//       write(1, " ", 1);
-//     } else {
-//       write(1, "\n", 1);
-//     }
-//   }
-  exit(0);
+    // close(0);
+    int fd;
+    fd = open("console",O_RDWR);
+    close(1);
+    printf("fd: %d",fd);
+    fprintf(fd,"test output\n");
+    fprintf(fd,"fffd: %d\n",fd);
+    exit(0);
 }
