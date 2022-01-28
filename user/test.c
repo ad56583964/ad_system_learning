2b62080665f97101ed72a845b2b198942069c2b6
ad56583964 <1176949486@qq.com>
2022-01-28 03:14:18 -0800

    test sh while console

--- user/test.c
+++ user/test.c
@@ -3,19 +3,35 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
-int haha()
+int
+getcmd(char *buf, int nbuf) //sizeof buf
 {
-    printf("Do\n");
+    fprintf(2, "buf[0]: %d\n",buf[0]);
+    fprintf(2, "@ "); // output to error?? 
+    memset(buf, 0, nbuf); //set?? full 0??
+    gets(buf, nbuf);  // gets??
+    if(buf[0] == 0) // EOF
+        return -1;
     return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
+    static char buf[100]; // the size is 100
+    // int fd;
+
+  // Ensure that three file descriptors are open.
+    // while((fd = open("console", O_RDWR)) >= 0){
+    //     if(fd >= 3){
+    //         close(fd);
+    //         break;
+    //     }
+    // }
     // char cache;
-    char buf[100];
+    while(getcmd(buf, sizeof(buf)) >= 0);
 
-    printf("The Size: %d", sizeof(buf));
-
+    printf("exit test\n");
+    while(1);
     exit(0);
 }
