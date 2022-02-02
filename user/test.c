6943824415f6054ad564bc9c3222056b456726c1
ad56583964 <1176949486@qq.com>
2022-02-02 13:12:35 -0800

    showCurrentFiles

--- user/test.c
+++ user/test.c
@@ -5,12 +5,40 @@
 
 #include "kernel/fs.h"
 
-void check_arg(char *arg[])
+void checkArg(char *arg[])
 {
     printf("*arg:%p\n",arg[2]);
 
 }
 
+void showCurrentFiles(const int fd)
+{
+    struct dirent de;
+    while(read(fd,&de,sizeof(de)) == sizeof(de))
+    {
+
+        // static int cnt = 0;
+        // cnt++;
+        //
+        struct stat fstat;
+        //get stat
+        // int is_dir = 0;
+        stat(de.name,&fstat);
+
+
+        if(de.inum == 0){
+            continue;
+        }
+       
+        //
+        // printf("CNT:%d  ",cnt);
+        printf("stat:%d  ",fstat.type);
+        printf("%s    ",de.name); //??符号的优先级 
+        printf("inum:%d\n",de.inum);
+        // printf("sizeof:%d",sizeof(de));
+    }
+    printf("finish print\n");
+}
 
 int
 main(int argc, char *argv[])//指针数组？？
@@ -19,30 +47,13 @@ main(int argc, char *argv[])//指针数组？？
     //review the is
     // "." is the current path
 // *p++ = '/';
-    struct dirent de;
 
     // char* p = "haha\n";
     int fd = open("./",O_RDONLY);
-    while(read(fd,&de,sizeof(de)) == sizeof(de))
-    {
-        static int cnt = 0;
-        cnt++;
-        // if(de.inum == 0){
-        //     continue;
-        // }
-        printf("CNT:%d  ",cnt);
-        printf("%s    ",de.name); //??符号的优先级 
-        printf("inum:%d\n",de.inum);
-        // printf("sizeof:%d",sizeof(de));
-    }
-    printf("finish write\n");
-    // int fd = open("./",O_RDONLY);
-    // for(int i = 0; i < 10; i++){
-    //     char ch;
-    //     read(fd,&ch,1);
-    //     write(1,&ch,1);
-    // }
-    // fd = open(argv)
-    // check_arg(argv);
+    showCurrentFiles(fd);
+
+    
+
+
     exit(0);
 }
