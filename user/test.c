390c5f213026c46a02024138806749df55274293
ad56583964 <1176949486@qq.com>
2022-02-03 07:46:52 -0800

    fix the struct sizeof

--- user/test.c
+++ user/test.c
@@ -9,13 +9,13 @@
 
 
 
-
+int getFile(int fd, struct dirent* file_info);
 void travelCurrentFiles(const char* current_dir_name,const int fd);
 void showString(char* str);
 void showCurrentFiles(int level,const char* current_dir_name,const int fd);
 void showStringLen(char* str);
 void showStrPtr(char* str);
-int getFile(int fd, struct dirent* file_info);
+void showFileInfo(struct dirent* file_info);
 void addpath(char* filenamef,char* add_dir);
 
 int
@@ -36,7 +36,25 @@ main(int argc, char *argv[])//指针数组？？
     // char* test = "hahahaha";
     // addpath(filename,test);
 
+
     exit(0);
+}
+
+int getFile(int fd, struct dirent* pfile_info)//??why??
+{
+    int check_read = read(fd,pfile_info,sizeof(struct dirent));
+    
+    if(check_read == sizeof(struct dirent)){
+        if(pfile_info->inum == 0){
+            return INUM_EMPTY;
+        }
+        else{
+            return (*pfile_info).inum;
+        }
+    }
+    else{
+        return 0;
+    }
 }
 
 void addpath(char* filenamef,char* add_dir) //filename_front
@@ -48,31 +66,13 @@ void addpath(char* filenamef,char* add_dir) //filename_front
     showString(filenamef);
 }
 
-void showFileInfo(struct dirent* file_info)
+void showFileInfo(struct dirent* pfile_info)
 {   
-    struct stat fstat;
+    // struct stat fstat;
 
-    stat(file_info->name,&fstat);
-    printf("%s",file_info->name); //??符号的优先级
-
-}
-
-int getFile(int fd, struct dirent* file_info)
-{
-    int check_read = read(fd,file_info,sizeof(file_info));
+    // stat(file_info->name,&fstat);
+    printf("%s \n",(*pfile_info).name); //??符号的优先级
     
-    if(check_read == sizeof(file_info)){
-        if(file_info->inum == 0){
-            return INUM_EMPTY;
-        }
-        else{
-            return file_info->inum;
-        }
-    }
-    else{
-        fprintf(2,"getFile failed\n");
-        return 0;
-    }
 }
 
 void travelCurrentFiles(const char* current_dir_name,const int fd)
@@ -81,10 +81,16 @@ void travelCurrentFiles(const char* current_dir_name,const int fd)
 
     printf("CurrentFile: %s\n",current_dir_name);
 
+    // while(read(fd,&file_info,sizeof(file_info)) == sizeof(file_info)) //get file info
+    // {
     while(getFile(fd,&file_info)) //get file info
-    {
-
+    {       
+        // if(file_info.inum == 0){
+        //     continue;
+        // }
         showFileInfo(&file_info);
+        // printf("%s ",file_info.name); //??符号的优先级
+
     }
     printf("finish print\n");
 }
