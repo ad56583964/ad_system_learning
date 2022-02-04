60c112daf5f071204f17da66048ad935692bcb17
ad56583964 <1176949486@qq.com>
2022-02-04 08:37:44 -0800

    need to add jump overlapping function

--- user/test.c
+++ user/test.c
@@ -10,7 +10,7 @@
 #define INUM_EMPTY 2
 
 int readFile(int fd, struct dirent* file_info);
-void travelCurrentFiles(const char* current_dir_name,const int fd);
+void travelCurrentFiles(const char* dir_name);
 void showString(char* str);
 void showCurrentFiles(int level,const char* current_dir_name,const int fd);
 void showStringLen(char* str);
@@ -18,20 +18,20 @@ void showStrPtr(char* str);
 void getFileInfo(const struct dirent* pfile_info,struct stat* pfstat);
 void addpath(char* filenamef,char* add_dir);
 
+void findStr(char* last_str,char* find_str);
+
 int isDir(struct dirent* current_dirent,struct stat* pfstat);
 
 int
 main(int argc, char *argv[]){//指针数组？？
 
-    char filename[128];
+    char last_str[] = "bb";
     // char* p = "haha\n";
+    char find_str[] = "aaaabbb";
+    findStr(last_str,find_str);
+    // strcpy(filename,".");
 
-    strcpy(filename,".");
-
-    int fd = open(filename,O_RDONLY);
-
-
-    travelCurrentFiles(filename,fd);
+    // travelCurrentFiles(argv[1]);
 
     exit(0);
 }
@@ -71,34 +71,40 @@ void getFileInfo(const struct dirent* pcurrent_dirent,struct stat* pfstat){
 }
 
 
-void travelCurrentFiles(const char* current_dir_name,const int fd){
+void travelCurrentFiles(const char* dir_name){
     struct dirent current_dirent;
     struct stat fstat;
-    char temp_dir_name[128];
 
+    char temp_dir_name[128]; // use this dir_name in this function
+    memcpy(temp_dir_name,dir_name,strlen(dir_name));
+    //decouple the current_dir_name
 
-    printf("CurrentFile: %s\n",current_dir_name);
+    int top_fd = open(temp_dir_name,O_RDONLY);
 
-    while(readFile(fd,&current_dirent)){ //get file info      
+    printf("CurrentFile: %s\n",temp_dir_name);
+
+    while(readFile(top_fd,&current_dirent)){ //get file info      
         if(current_dirent.inum == 0){
             continue;
         }
 
         getFileInfo(&current_dirent,&fstat);
+        //add custom code
+            //process the dirent.name
+            // current_dirent.name
+            // strcmp
+        //end
 
         if(isDir(&current_dirent,&fstat)){
             //change file string
             fprintf(2,"find a dir\n");
             fprintf(2,"%c\n",current_dirent.name[0]);
 
-            // copy dir_name
-            memcpy(temp_dir_name,current_dir_name,strlen(current_dir_name));
             // "xx" + "/xx"
             addpath(temp_dir_name,current_dirent.name);
 
             // into the child dir
-            int fd = open(temp_dir_name,O_RDONLY);
-            travelCurrentFiles(temp_dir_name,fd);
+            travelCurrentFiles(temp_dir_name);
         }
     }
     printf("finish print\n");
@@ -130,4 +136,41 @@ void showStringLen(char* str)
 void showStrPtr(char* str)
 {
     printf("StrPtr:%p\n",str);
+}
+
+void findStr(char* last_str,char* find_str){
+
+    char* pfind_str_end = find_str + strlen(find_str);
+
+    int form_str = strlen(last_str);
+    int find_length = 0;
+    printf("start_find\n");
+    //move the point in pfind
+    char* pfind_str_point = find_str;
+    char* plast_str_point = last_str;
+    for(pfind_str_point = find_str;pfind_str_point <= (pfind_str_end - form_str); pfind_str_point++){ //?? //have overlapping object
+        ////////////////////////////////////////////////it's not easy to understand
+        
+        for(plast_str_point = last_str; plast_str_point < last_str + form_str; plast_str_point++){
+
+            if(*plast_str_point == *pfind_str_point){
+                printf("Fit:%c\n",*pfind_str_point);
+                // while(1);
+                find_length++;
+                printf("find_length:%d\n",find_length);
+                if(find_length == form_str)
+                {
+                    printf("Find!\n");
+                    find_length = 0;
+                    // while(1);
+                }
+            }
+            else{
+                find_length = 0;
+                continue;
+            }
+        }
+
+    }
+
 }
