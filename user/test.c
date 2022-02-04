398e2b46c0b83449c6b899567bb34fb7592398a2
ad56583964 <1176949486@qq.com>
2022-02-04 09:34:53 -0800

    快了快了

--- user/test.c
+++ user/test.c
@@ -8,30 +8,31 @@
 #include "ad_tools.h"
 
 #define INUM_EMPTY 2
+#define PRINT_ERROR 2
 
 int readFile(int fd, struct dirent* file_info);
-void travelCurrentFiles(const char* dir_name);
+int findCurrentFiles(const char* dir_name, char* fit_str);
 void showString(char* str);
-void showCurrentFiles(int level,const char* current_dir_name,const int fd);
+// void showCurrentFiles(int level,const char* current_dir_name,const int fd);
 void showStringLen(char* str);
 void showStrPtr(char* str);
 void getFileInfo(const struct dirent* pfile_info,struct stat* pfstat);
 void addpath(char* filenamef,char* add_dir);
 
-void findStr(char* last_str,char* find_str);
+int findStr(char* last_str,char* find_str);
 
 int isDir(struct dirent* current_dirent,struct stat* pfstat);
 
 int
 main(int argc, char *argv[]){//指针数组？？
 
-    char last_str[] = "bb";
+    // char last_str[] = "hab";
     // char* p = "haha\n";
-    char find_str[] = "aaaabbb";
-    findStr(last_str,find_str);
+    // char find_str[] = "aaahabaaahabhabhbbb";
+    // printf("find:%d\n",findStr(last_str,find_str));
     // strcpy(filename,".");
-
-    // travelCurrentFiles(argv[1]);
+    close(2);
+    findCurrentFiles(argv[1],argv[2]);
 
     exit(0);
 }
@@ -66,12 +67,12 @@ void getFileInfo(const struct dirent* pcurrent_dirent,struct stat* pfstat){
     //this logic must touch the first invalid
     stat(pcurrent_dirent->name,pfstat);
     //output
-    printf("Name:%s \n",(*pcurrent_dirent).name); //??符号的优先级
-    printf("type:%d \n",(*pfstat).type);    //
+    fprintf(PRINT_ERROR,"Name:%s \n",(*pcurrent_dirent).name); //??符号的优先级
+    fprintf(PRINT_ERROR,"type:%d \n",(*pfstat).type);    //
 }
 
-
-void travelCurrentFiles(const char* dir_name){
+int findCurrentFiles(const char* dir_name, char* fit_str){
+    int cnt = 0;
     struct dirent current_dirent;
     struct stat fstat;
 
@@ -93,22 +94,66 @@ void travelCurrentFiles(const char* dir_name){
             //process the dirent.name
             // current_dirent.name
             // strcmp
+        if(findStr(fit_str,current_dirent.name)){
+            cnt++;
+        }
         //end
 
         if(isDir(&current_dirent,&fstat)){
             //change file string
-            fprintf(2,"find a dir\n");
-            fprintf(2,"%c\n",current_dirent.name[0]);
+            fprintf(PRINT_ERROR,"find a dir\n");
+            fprintf(PRINT_ERROR,"%c\n",current_dirent.name[0]);
 
             // "xx" + "/xx"
             addpath(temp_dir_name,current_dirent.name);
 
             // into the child dir
-            travelCurrentFiles(temp_dir_name);
+            cnt += findCurrentFiles(temp_dir_name,fit_str);
         }
     }
-    printf("finish print\n");
+    fprintf(PRINT_ERROR,"finish print\n");
+    fprintf(PRINT_ERROR,"CNT:%d",cnt);
+    return cnt;
 }
+
+// void travelCurrentFiles(const char* dir_name){
+//     struct dirent current_dirent;
+//     struct stat fstat;
+
+//     char temp_dir_name[128]; // use this dir_name in this function
+//     memcpy(temp_dir_name,dir_name,strlen(dir_name));
+//     //decouple the current_dir_name
+
+//     int top_fd = open(temp_dir_name,O_RDONLY);
+
+//     printf("CurrentFile: %s\n",temp_dir_name);
+
+//     while(readFile(top_fd,&current_dirent)){ //get file info      
+//         if(current_dirent.inum == 0){
+//             continue;
+//         }
+
+//         getFileInfo(&current_dirent,&fstat);
+//         //add custom code
+//             //process the dirent.name
+//             // current_dirent.name
+//             // strcmp
+//         //end
+
+//         if(isDir(&current_dirent,&fstat)){
+//             //change file string
+//             fprintf(2,"find a dir\n");
+//             fprintf(2,"%c\n",current_dirent.name[0]);
+
+//             // "xx" + "/xx"
+//             addpath(temp_dir_name,current_dirent.name);
+
+//             // into the child dir
+//             travelCurrentFiles(temp_dir_name);
+//         }
+//     }
+//     printf("finish print\n");
+// }
 
 int isDir(struct dirent* current_dirent,struct stat* pfstat){
     if(current_dirent->name[0] == '.'){
@@ -125,52 +170,59 @@ int isDir(struct dirent* current_dirent,struct stat* pfstat){
 void showString(char* str)
 {
 
-    printf("Str:%s\n",str);
+    fprintf(PRINT_ERROR,"Str:%s\n",str);
 }
 
 void showStringLen(char* str)
 {
-    printf("StrLen:%d\n",strlen(str));
+    fprintf(PRINT_ERROR,"StrLen:%d\n",strlen(str));
 }
 
 void showStrPtr(char* str)
 {
-    printf("StrPtr:%p\n",str);
+    fprintf(PRINT_ERROR,"StrPtr:%p\n",str);
 }
 
-void findStr(char* last_str,char* find_str){
-
+int findStr(char* last_str,char* find_str){
+    int find_flag = 0;
     char* pfind_str_end = find_str + strlen(find_str);
 
     int form_str = strlen(last_str);
     int find_length = 0;
-    printf("start_find\n");
+    // printf("start_find\n");
     //move the point in pfind
     char* pfind_str_point = find_str;
     char* plast_str_point = last_str;
-    for(pfind_str_point = find_str;pfind_str_point <= (pfind_str_end - form_str); pfind_str_point++){ //?? //have overlapping object
+    for(pfind_str_point = find_str;pfind_str_point < (pfind_str_end - form_str); pfind_str_point++){ //?? //have overlapping object
         ////////////////////////////////////////////////it's not easy to understand
         
+        char* temp_point = pfind_str_point;
         for(plast_str_point = last_str; plast_str_point < last_str + form_str; plast_str_point++){
-
-            if(*plast_str_point == *pfind_str_point){
-                printf("Fit:%c\n",*pfind_str_point);
+            // printf("%c",*plast_str_point);
+            if(*plast_str_point == *temp_point){
+                // printf("Fit:%c\n",*temp_point);
                 // while(1);
                 find_length++;
-                printf("find_length:%d\n",find_length);
+                // printf("find_length:%d\n",find_length);
                 if(find_length == form_str)
                 {
-                    printf("Find!\n");
+                    find_flag++;
+                    printf("          Find!\n");
                     find_length = 0;
-                    // while(1);
+                    pfind_str_point += (form_str-1);//jump the overlapping bit //should consider the following ++
+                    // printf("Now: %c",*pfind_str_point);
+                    
+                    break;
                 }
             }
             else{
                 find_length = 0;
-                continue;
             }
+            temp_point++;
         }
 
     }
+
+    return find_flag;
 
 }
